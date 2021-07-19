c5d431c8aac446dfe627f839a9cd4b52cce9bba9
seongjuseo <canwefly89@gmail.com>
2021-07-19 18:11:16 +0900

    Add: React Suspense & Loading Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Loading from '../components/Loading';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Suspense fallback={Loading}>
+      <Component {...pageProps} />;
+    </Suspense>
+  );
 };
 
 export default MyApp;
