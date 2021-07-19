66f4fc398d663ef965e39620a6eb648bc07697c8
seongjuseo <canwefly89@gmail.com>
2021-07-19 19:52:35 +0900

    Add: RelayEnvironmentProvider and related logics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import React, { Suspense } from 'react';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import { getInitialPreloadedQuery, getRelayProps } from 'relay-nextjs/app';
+import { getClientEnvironment } from '../lib/relay_client_environment';
 import Loading from '../components/Loading';
 
+// const Environment = createNewEnvironment();
+const clientEnv = getClientEnvironment();
+const initialPreloadedQuery = getInitialPreloadedQuery({
+  createClientEnvironment: () => getClientEnvironment()!,
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const relayProps = getRelayProps(pageProps, initialPreloadedQuery);
+  const environment = relayProps.preloadedQuery?.environment ?? clientEnv!;
+
   return (
-    <Suspense fallback={Loading}>
-      <Component {...pageProps} />;
-    </Suspense>
+    <RelayEnvironmentProvider environment={environment}>
+      <Suspense fallback={Loading}>
+        <Component {...pageProps} />;
+      </Suspense>
+    </RelayEnvironmentProvider>
   );
 };
 
