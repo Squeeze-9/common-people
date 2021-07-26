abf7c2f7ad29e8c09b52d5248c2572b4e9805c69
seongjuseo <canwefly89@gmail.com>
2021-07-26 21:40:33 +0900

    Delete: remove getStaticProp (not page component)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import AppLayout from '../components/layout/AppLayout';
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 
 const Login: React.FunctionComponent = () => {
   return <AppLayout>Login</AppLayout>;
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  console.log(context);
+  console.log(process.cwd());
+  return {
+    props: {},
+    revalidate: 10,
+  };
 };
 
 export default Login;
