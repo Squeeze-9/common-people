43ccfd4c6cc6f3ccca772787e3893154d8fffde3
seongjuseo <canwefly89@gmail.com>
2021-07-26 21:39:06 +0900

    Delete: remove getStaticProp from Footer (not page component)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { GetStaticProps, GetStaticPropsContext } from 'next';
 
 const Footer: React.FunctionComponent = () => {
-  // console.log('static footer');
   return (
     <div>
       <h1>Footer!</h1>
     </div>
   );
-};
-
-export const getStaticProps: GetStaticProps = async (
-  context: GetStaticPropsContext
-) => {
-  console.log(context);
-  console.log(process.cwd());
-  return {
-    props: {},
-    revalidate: 100,
-  };
 };
 
 export default Footer;
