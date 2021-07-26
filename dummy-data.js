3d47c51ef16408d9b98fd989ec7fc417566af8a0
seongjuseo <canwefly89@gmail.com>
2021-07-26 21:37:56 +0900

    Add: make postDetail page with mock-data

--- dummy-data.js
+++
@@ -1,6 +0,0 @@
-export const dummyPost = {
-  title: 'dummy Title1',
-  description: 'description',
-};
-
-export const dummyUser = {};
--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,24 +1,81 @@
 import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
 import React from 'react';
+import fs from 'fs/promises';
+import path from 'path';
 
-const PostDetail: React.FunctionComponent = () => {
+type Post = {
+  postId: string;
+  author: string;
+  title: string;
+  description: string;
+  comments: [string];
+  likeCount: number;
+  dislikeCount: number;
+  viewCount: number;
+};
+
+type PostDetailProps = {
+  post: Post;
+};
+
+const PostDetail: React.FC<PostDetailProps> = ({ post }) => {
   const {
-    query: { postId },
-  } = useRouter();
-  console.log('serverside postDetail');
+    title,
+    description,
+    comments,
+    likeCount,
+    dislikeCount,
+    viewCount,
+  } = post;
 
   return (
     <div>
       <h1>PostDetail</h1>
-      <p>{postId}</p>
+      <h2>제목: {title}</h2>
+      <p>내용: {description}</p>
+      <p>
+        댓글:
+        {comments.map((comment, index) => (
+          <span key={index}>{comment}</span>
+        ))}
+      </p>
+      <p>조회 수: {viewCount}</p>
+      <p>좋아요 수: {likeCount}</p>
+      <p>싫어요 수: {dislikeCount}</p>
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+  const filePath: string = path.join(process.cwd(), 'data', 'dummy-data.json');
+  const jsonData = await fs.readFile(filePath, 'utf-8');
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
+  const post = data.posts.find(
+    (post: Post): boolean => post.postId === params?.postId
+  );
+
+  if (!post) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
   return {
-    props: {},
+    props: { post },
   };
 };
 
